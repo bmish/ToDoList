9199c301e3bad88d987120499639d6c9890e2be9
Bryan Mishkin <bryan.mishkin@gmail.com>
2014-04-22 21:57:50 -0700

    Added a dynamic status field to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,13 +9,27 @@ class Task < ActiveRecord::Base
   validates_uniqueness_of :title, scope: :list_id, :case_sensitive => false
   validate :due_date_cannot_be_after_start_date
 
+  def status
+    if self.blocked
+      return 'Blocked'
+    elsif self.start && self.start > Date.today
+      return 'Waiting'
+    elsif self.start && self.due && Date.today > self.start && Date.today < self.due
+      return 'Actionable'
+    elsif self.due && Date.today > self.due
+      return 'Overdue'
+    else
+      return 'Actionable'
+    end
+  end
+
   private
     def remove_whitespace
       self.title = self.title ? self.title.strip : ""
     end
 
     def due_date_cannot_be_after_start_date
-      if start.present? && due.present? && start > due
+      if self.start.present? && self.due.present? && self.start > self.due
         errors.add(:start, "date cannot be after the due date.")
       end
     end
